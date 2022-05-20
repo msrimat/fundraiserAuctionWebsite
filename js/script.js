92ca0823cd263eb16a0bc3003c5d7314ce27ec7e
Manu Srimat <msrimat@gmail.com>
2022-05-20 14:37:56 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,62 @@
+// JavaScript Document
+
+$(document).ready(function() {
+	// navigation
+	document.addEventListener('click',function(e){
+		// Hamburger menu
+		if(e.target.classList.contains('hamburger-toggle')){
+		  e.target.children[0].classList.toggle('active');
+		}
+	});
+
+	$(' .hamburger').click(function () {
+		$('.header').toggleClass('open');
+	});
+
+	// // gallery
+	// $('.grid').isotope({
+	// 	itemSelector: '.grid-item',
+	// 	masonry: {
+	// 	//   columnWidth: 100,
+	// 	//   gutter: 20
+	// 	}
+	// });
+  // end
+
+	// scrollIt
+	$.scrollIt({
+		upKey: 38,             // key code to navigate to the next section
+		downKey: 40,           // key code to navigate to the previous section
+		easing: 'linear',      // the easing function for animation
+		scrollTime: 600,       // how long (in ms) the animation takes
+		activeClass: 'active', // class given to the active nav element
+		onPageChange: null,    // function(pageIndex) that is called when page is changed
+		topOffset: -101           // offste (in px) for fixed top navigation
+	}); 	
+
+
+	// Masonry
+	if($(".grid").length > 0) {
+		$('.grid').isotope({
+			// options...
+			itemSelector: '.grid .grid-item',
+			// masonry: {
+			//   columnWidth: 200
+			// }
+		});
+
+		// Image Loadead Masonry
+		var $grid = $('.grid').isotope({
+			// options...
+		});
+		// layout Isotope after each image loads
+		$grid.imagesLoaded().progress(function () {
+			$grid.isotope('layout');
+		});
+	}
+	// Masonry
+
+	// OFI Browser
+	objectFitImages();
+
+});
